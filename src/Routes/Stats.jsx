fb8e7cdc9b4a0205cdc76aa35018ff69d1c99f79
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-05-24 19:24:40 +0530

    stats

---
+++ src/Routes/Stats.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const Stats = () => {
+  return <div>Stats</div>;
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = (dispatch) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Stats);
