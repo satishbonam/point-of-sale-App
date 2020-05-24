6d521becb2798ef0aa60b813643d6a001878597b
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-05-24 19:24:49 +0530

    stocks

---
+++ src/Routes/Stocks.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const Stats = () => {
+  return <div>Stocks</div>;
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = (dispatch) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Stats);
