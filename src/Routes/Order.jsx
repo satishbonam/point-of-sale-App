3d1df73865e9aac31e6305b6c0c59df6e8dbb854
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-05-24 19:24:09 +0530

    Order

---
+++ src/Routes/Order.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const Stats = () => {
+  return <div>Order stock</div>;
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = (dispatch) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Stats);
