8e76a710d5b177d79f918ab952c9429222a65d78
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-05-24 19:24:15 +0530

    routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./Home";
 import { connect } from "react-redux";
+import NavBar from "../Components/NavBar";
+import Stats from "./Stats";
+import Stocks from "./Stocks";
+import Order from "./Order";
+import DashBoard from "./DashBoard";
 
 const Routes = (props) => {
   return (
     <div>
+      <NavBar />
       <Switch>
-        <Route path="/" exact render={(props) => <Home {...props} />} />
+        <Route path="/" exact render={(props) => <DashBoard {...props} />} />
+        <Route path="/stats" exact render={(props) => <Stats {...props} />} />
+        <Route path="/stocks" exact render={(props) => <Stocks {...props} />} />
+        <Route path="/order" exact render={(props) => <Order {...props} />} />
       </Switch>
     </div>
   );
