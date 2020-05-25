663f39ab5107cc258959cc77f15f397c69f44d44
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-05-25 18:09:12 +0530

    order

--- src/Routes/Order.jsx
+++ src/Routes/Order.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import DisplayTable from "../Components/common/DisplayTable";
+import { getOderData } from "../Redux/reducer/actions";
 
-const Stats = () => {
-  return <div>Order stock</div>;
+const Order = (props) => {
+  const { orderData, getOrderData } = props;
+  useEffect(() => {
+    getOrderData();
+  }, []);
+  return (
+    <div>
+      <DisplayTable rows={orderData} />
+    </div>
+  );
 };
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  orderData: state.reducer.orderData,
+});
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  getOrderData: (payload) => dispatch(getOderData(payload)),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Stats);
+export default connect(mapStateToProps, mapDispatchToProps)(Order);
