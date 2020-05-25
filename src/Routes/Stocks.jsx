7627f3ed56c78e90cc05e7b0ec990d1ecdfb1c13
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-05-25 18:09:28 +0530

    stocks

--- src/Routes/Stocks.jsx
+++ src/Routes/Stocks.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import DisplayTable from "../Components/common/DisplayTable";
+import { getData } from "../Redux/reducer/actions";
 
-const Stats = () => {
-  return <div>Stocks</div>;
+const Stocks = (props) => {
+  const { data, getData } = props;
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div>
+      <DisplayTable rows={data} />
+    </div>
+  );
 };
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  data: state.reducer.data,
+});
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  getData: (payload) => dispatch(getData(payload)),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Stats);
+export default connect(mapStateToProps, mapDispatchToProps)(Stocks);
