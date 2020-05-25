70b7e73efe254e856562b1faf1e44f2ebf198759
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-05-25 18:08:45 +0530

    actions

--- src/Redux/reducer/actions.js
+++ src/Redux/reducer/actions.js
@@ -9,6 +9,12 @@ import {
   GENERATE_BILL_REQUEST,
   GENERATE_BILL_SUCCESS,
   BILL_STATUS,
+  GET_ORDER_DATA_FAILURE,
+  GET_ORDER_DATA_REQUEST,
+  GET_ORDER_DATA_SUCCESS,
+  GET_BILLS_FAILURE,
+  GET_BILLS_REQUEST,
+  GET_BILLS_SUCCESS,
 } from "./actionTypes";
 import axios from "axios";
 
@@ -79,3 +85,51 @@ export const changeBillStatus = (payload) => ({
   type: BILL_STATUS,
   payload,
 });
+export const getOrderDataRequest = (payload) => ({
+  type: GET_ORDER_DATA_REQUEST,
+  payload,
+});
+
+export const getOrderDataSuccess = (payload) => ({
+  type: GET_ORDER_DATA_SUCCESS,
+  payload,
+});
+
+export const getOrderDataFailure = (payload) => ({
+  type: GET_ORDER_DATA_FAILURE,
+  payload,
+});
+
+export const getOderData = (payload) => (dispatch) => {
+  dispatch(getOrderDataRequest());
+  return axios({
+    url: "http://127.0.0.1:5000/order",
+    method: "get",
+  })
+    .then((res) => dispatch(getOrderDataSuccess(res.data)))
+    .catch((err) => dispatch(getOrderDataFailure(err)));
+};
+export const getBillsRequest = (payload) => ({
+  type: GET_BILLS_REQUEST,
+  payload,
+});
+
+export const getBillsSuccess = (payload) => ({
+  type: GET_BILLS_SUCCESS,
+  payload,
+});
+
+export const getBillsFailure = (payload) => ({
+  type: GET_BILLS_FAILURE,
+  payload,
+});
+
+export const getBills = (payload) => (dispatch) => {
+  dispatch(getBillsRequest());
+  return axios({
+    url: "http://127.0.0.1:5000/bills",
+    method: "get",
+  })
+    .then((res) => dispatch(getBillsSuccess(res.data)))
+    .catch((err) => dispatch(getBillsFailure(err)));
+};
