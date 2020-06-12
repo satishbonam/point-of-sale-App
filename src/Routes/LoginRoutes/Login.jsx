ed37123521b5734a6a986d7a5e3a6e32d8788ca7
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-06-13 00:50:19 +0530

    Updated:Routes folder structure

---
+++ src/Routes/LoginRoutes/Login.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+const Login = () => {
+  return <div>login</div>;
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
