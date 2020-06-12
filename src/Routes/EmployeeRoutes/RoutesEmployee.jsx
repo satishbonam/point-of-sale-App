68d600b0012ee12ae1179f850a17e220f905e825
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-06-13 00:49:59 +0530

    Updated:Routes folder structure

---
+++ src/Routes/EmployeeRoutes/RoutesEmployee.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import NavBar from "../../Components/NavBar";
+import Stats from "./Stats";
+import Stocks from "./Stocks";
+import Order from "./Order";
+import DashBoard from "./DashBoard";
+
+const RoutesEmployee = (props) => {
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact render={(props) => <DashBoard {...props} />} />
+        <Route path="/stats" exact render={(props) => <Stats {...props} />} />
+        <Route path="/stocks" exact render={(props) => <Stocks {...props} />} />
+        <Route path="/order" exact render={(props) => <Order {...props} />} />
+      </Switch>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RoutesEmployee);
