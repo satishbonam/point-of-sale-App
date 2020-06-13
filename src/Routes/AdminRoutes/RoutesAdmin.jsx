45937987f068b4534233111884742a36504c1f76
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-06-13 08:25:49 +0530

    Added:added admin routes

--- src/Routes/AdminRoutes/RoutesAdmin.jsx
+++ src/Routes/AdminRoutes/RoutesAdmin.jsx
@@ -1,21 +1,16 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import NavBar from "../../Components/NavBar";
-import Stats from "./Stats";
-import Stocks from "./Stocks";
-import Order from "./Order";
-import DashBoard from "./DashBoard";
+import Stats from "../EmployeeRoutes/Stats";
 
 const RoutesAdmin = (props) => {
+  const history = useHistory();
   return (
     <div>
-      <NavBar />
       <Switch>
-        <Route path="/" exact render={(props) => <DashBoard {...props} />} />
-        <Route path="/stats" exact render={(props) => <Stats {...props} />} />
-        <Route path="/stocks" exact render={(props) => <Stocks {...props} />} />
-        <Route path="/order" exact render={(props) => <Order {...props} />} />
+        <Route path="/admin" exact render={(props) => <Stats {...props} />} />
+        <Route path="" render={() => <Redirect to="/admin" />} />
       </Switch>
     </div>
   );
