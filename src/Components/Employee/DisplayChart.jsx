601ad48e015ff6a79e4f1ac12dbfdadaa4f86914
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-06-13 11:11:57 +0530

    Updated:folder structure for components

---
+++ src/Components/Employee/DisplayChart.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import HighchartsReact from "highcharts-react-official";
+import Highcharts from "highcharts";
+import { connect } from "react-redux";
+import { getBills } from "../../Redux/Employee/actions";
+
+const DisplayChart = (props) => {
+  const [hoverData, setHoverData] = useState(null);
+  const { getBills, billData } = props;
+  console.log(billData);
+  useEffect(() => {
+    getBills();
+  }, []);
+  const chartOptions = {
+    xAxis: {
+      categories: billData && billData.y,
+    },
+    yAxis: {
+      title: {
+        text: "Amount",
+      },
+    },
+    title: {
+      text: "Today Bussiness",
+    },
+    series: [{ data: billData && billData.x, name: "Today Sale" }],
+    plotOptions: {
+      series: {
+        point: {
+          events: {
+            mouseOver(e) {
+              setHoverData(e.target.category);
+            },
+          },
+        },
+      },
+    },
+  };
+
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+      {/* <button onClick={updateSeries}>Update Series</button> */}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  billData: state.employee.billData,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getBills: (payload) => dispatch(getBills(payload)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(DisplayChart);
